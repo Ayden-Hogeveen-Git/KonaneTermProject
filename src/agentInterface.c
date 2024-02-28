7cddb4c68388ca38373430279467a9c6a4b96fb7
Kevin Ulliac <kevanoullio@outlook.com>
2024-02-27 20:45:06 -0700

    Mostly finished agentInterface.c, bugs with opening and closing text file representing game board state.

--- src/agentInterface.c
+++ src/agentInterface.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "structures.h"
+#include "konane.h"
 #include "minimaxAgent.h"
 
 
@@ -10,10 +12,10 @@ int whoseTurn(GameState* game) {
 
     for (int y = 8; y > 0; y--) {
         for (int x = 0; x < 8; x++) {
-            if (game->board[y-1][x].piece == 'B') {
+            if (game->board[y - 1][x].piece == BLACK) {
                 black++;
             }
-            else if (game->board[y-1][x].piece == 'W') {
+            else if (game->board[y - 1][x].piece == WHITE) {
                 white++;
             }
         }
@@ -27,7 +29,40 @@ int whoseTurn(GameState* game) {
     }
 }
 
-GameState* initalizeGame(char* game) {
+char* formatGameString(char* gameString) {
+    // Calculate the length of the gameString, excluding the newline characters
+    int length = 0;
+    for (int i = 0; gameString[i] != '\0'; i++) {
+        if (gameString[i] != '\n') {
+            length++;
+        }
+    }
+
+    // Allocate memory for the new game string
+    char* newGameString = malloc(length + 1); // +1 for the null terminator
+
+    // Check if memory allocation failed or not
+    if (newGameString == NULL) {
+        printf("Memory allocation failed\n");
+        exit(1);
+    }
+
+    // Remove the newline characters from the game string
+    int j = 0;
+    for (int i = 0; i < length; i++) {
+        if (gameString[i] != '\n') {
+            newGameString[j] = gameString[i];
+            j++;
+        }
+    }
+
+    // Add the null terminator to the end of the string
+    newGameString[j] = '\0';
+
+    return newGameString;
+}
+
+GameState* initalizeGame(char* gameString) {
     // Allocate memory for the new game
     GameState* newGame = malloc(sizeof(GameState));
 
@@ -37,12 +72,36 @@ GameState* initalizeGame(char* game) {
         exit(1);
     }
 
+    // Format the game string
+    char* newGameString = formatGameString(gameString);
+
+    printf("%s\n", newGameString);
+
+
     // Initialize the game board
     for (int y = 8; y > 0; y--) {
         for (int x = 0; x < 8; x++) {
-            newGame->board[y-1][x].piece = game[(8-y)*8 + x];
-            newGame->board[y-1][x].position.x = x;
-            newGame->board[y-1][x].position.y = y;
+            // Convert the character to a piece
+            if (newGameString[(8 - y) * 8 + x] == 'B') {
+                newGame->board[y - 1][x].piece = BLACK;
+            }
+            else if (newGameString[(8 - y)*8 + x] == 'W') {
+                newGame->board[y - 1][x].piece = WHITE;
+            }
+            else if (newGameString[(8 - y)*8 + x] == 'O') {
+                newGame->board[y - 1][x].piece = EMPTY;
+            }
+            else if (newGameString[(8 - y)*8 + x] == '\0') {
+                break;
+            }
+            else {
+                printf("Invalid character in game string\n");
+                exit(1);
+            }
+
+            // Set the position of the piece
+            newGame->board[y - 1][x].position.x = x;
+            newGame->board[y - 1][x].position.y = y;
         }
     }
 
@@ -54,6 +113,9 @@ GameState* initalizeGame(char* game) {
 
     // Set the winner to empty
     newGame->winner = EMPTY;
+
+    // Free the memory
+    free(newGameString);
 
     // Return the new game
     return newGame;
@@ -71,6 +133,72 @@ void agentOutput(Move move) {
 	}
 }
 
-int main() {
+int main(int argc, char* argv[]) {
+    // Initialize the game string
+    char* gameString = malloc(72); // 64 (8 x 8) + 7 newline characters + 1 null terminator
+    if (gameString == NULL) {
+        printf("Memory allocation failed\n");
+        return 1;
+    }
 
+    // Check if the user provided a file
+    if (argc == 2) {
+        // Initialize the file
+        FILE* file;
+
+        // Open the file
+        file = fopen(argv[1], "r");
+        if (file == NULL) {
+            printf("File not found\n");
+            return 1;
+        }
+
+        // Read all lines of the game from the file, including the newline characters
+        char c;
+        int i = 0;
+        while ((c = fgetc(file)) != EOF) {
+            gameString[i] = c;
+            i++;
+        }
+
+        // Close the file
+        if (file != NULL) {
+            fclose(file);
+        }
+
+    } else if (argc == 1) {
+        // Define a default new game string
+        const char* defaultGameString = "BWBWBWBW\n"
+                                        "WBWBWBWB\n"
+                                        "BWBWBWBW\n"
+                                        "WBWBWBWB\n"
+                                        "BWBWBWBW\n"
+                                        "WBWBWBWB\n"
+                                        "BWBWBWBW\n"
+                                        "WBWBWBWB";
+
+        // Copy the default game string to the game string
+        strcpy(gameString, defaultGameString);
+
+    } else {
+        printf("Usage: %s [file]\n", argv[0]);
+        return 1;
+    }
+
+    // Initialize the game
+    GameState* game = initalizeGame(gameString);
+
+    // printf("%s\n", gameString);
+    printBoard(*game);
+    // Get the next move
+    Move move = minimaxAlphaBeta(game);
+
+    // Output the move
+    agentOutput(move);
+
+    // Free the memory
+    free(gameString);
+    free(game);
+
+    return 0;
 }
